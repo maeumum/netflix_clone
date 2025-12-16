cc0471f410cd206d1d98b0fce672e9950628bc47
maeumum <maeum0707@gmail.com>
2025-12-16 22:53:22 +0900

    httpclinet setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-// 👇 우리가 만든 설정을 가져옵니다.
 import { appConfig } from './app/app.config';
 
-// 👇 appConfig를 사용해서 앱을 시작합니다.
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
